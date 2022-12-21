7c726f345748e016ed5aec0be364f0098dfe0c15
Franklyn <franklyn1998@live.com>
2022-12-21 15:42:43 -0300

    Project create

---
+++ src/main/java/com/github/hadesfranklyn/project/BookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.hadesfranklyn.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+}
