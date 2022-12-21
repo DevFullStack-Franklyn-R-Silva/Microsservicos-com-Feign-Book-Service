a55ef60a1682e67609cdcae835d4a27de55a7020
Franklyn <franklyn1998@live.com>
2022-12-21 16:12:56 -0300

    Integrando o Book Service ao Banco de Dados com JPA e Spring Data

--- src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
+++ src/main/java/com/github/hadesfranklyn/project/controller/BookController.java
@@ -1,7 +1,5 @@
 package com.github.hadesfranklyn.project.controller;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,25 +8,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.hadesfranklyn.project.model.Book;
+import com.github.hadesfranklyn.project.repository.BookRepository;
 
 @RestController
 @RequestMapping("book-service")
 public class BookController {
-	
+
 	@Autowired
 	private Environment environment;
-	
+
+	@Autowired
+	private BookRepository repository;
+
 	// http://localhost:8100/book-service/1/BRL
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(
-			@PathVariable("id") Long id,
+			@PathVariable("id") Long id, 
 			@PathVariable("currency") String currency
 			) {
-		
+
+		var book = repository.getById(id);
+		if (book == null)
+			throw new RuntimeException("Book not found");
+
 		var port = environment.getProperty("local.server.port");
-		
-		return new Book(1L, "Nigel poulton","Docker Deep Dive", new Date(),
-				Double.valueOf(13.7), currency, port);
+		book.setEnviroment(port);
+		return book;
 	}
-	
+
 }
