9e3cdb29d42606f0ef197d932087ee013209b83f
Franklyn <franklyn1998@live.com>
2022-12-21 16:02:23 -0300

    Adicionando o Flyway e as Dependências do Spring Data JPA ao Book Service

--- src/main/java/com/github/hadesfranklyn/project/model/Book.java
+++ src/main/java/com/github/hadesfranklyn/project/model/Book.java
@@ -3,18 +3,44 @@ package com.github.hadesfranklyn.project.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+
+@Entity
 public class Book implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "author", nullable = false , length = 180)
 	private String author;
+	
+	@Column(name = "launch_date", nullable = false)
+	@Temporal(TemporalType.DATE)
 	private Date launchDate;
+	
+	@Column(nullable = false)
 	private Double price;
+	
+	@Column(nullable = false, length = 250)
 	private String title;
+	
+	@Transient
 	private String currency;
+	
+	@Transient
 	private String enviroment;
 
+	// constructors
 	public Book() {
 	}
 
@@ -30,7 +56,8 @@ public class Book implements Serializable {
 		this.currency = currency;
 		this.enviroment = enviroment;
 	}
-
+	
+	// gets and sets
 	public Long getId() {
 		return id;
 	}
